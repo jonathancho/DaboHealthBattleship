e4a3a031ac932c143195413c31d03f4c74f979b2
Jonathan Cho <jcho@playnetwork.com>
2014-05-03 21:19:09 -0700

    done but missing validation for moves and missing labels for axis

--- Server/server.rb
+++ Server/server.rb
@@ -29,6 +29,17 @@ class Match
 		@numboats1 = 5
 		@numboats2 = 5
 
+		@carrier1 = 5
+		@battleship1 = 4
+		@submarine1 = 3
+		@destroyer1 = 3
+		@patrolboat1 = 2
+		@carrier2 = 5
+		@battleship2 = 4
+		@submarine2 = 3
+		@destroyer2 = 3
+		@patrolboat2 = 2
+
 		for i in 0..9 do
 			for j in 0..9 do
 				@board1[i][j] = '-'
@@ -89,11 +100,69 @@ class Match
 		if @board2[row][column] == '-'
 			@player1.puts "Shot Missed!"
 			@player2.puts "Player 1 shot at #{input1} and missed!"
-		else
+		elsif @board2[row][column] == 'c'
 			@player1.puts "Shot Hit!"
 			@player2.puts "Player 1 shot at #{input1} and hit!"
+			@carrier2 -= 1
+
+			if @carrier2 == 0
+				@player1.puts "Player 2's Carrier has been sunk!"
+				@player2.puts "Your Carrier has been destroyed!"
+				@numboats2
+			end
+		elsif @board2[row][column] == 'b'
+			@player1.puts "Shot Hit!"
+			@player2.puts "Player 1 shot at #{input1} and hit!"
+
+			@battleship2 -= 1
+
+			if @battleship2 == 0
+				@player1.puts "Player 2's Battleship has been sunk!"
+				@player2.puts "Your Battleship has been destroyed!"
+				@numboats2 -= 1
+			end
+		elsif @board2[row][column] == 's'
+			@player1.puts "Shot Hit!"
+			@player2.puts "Player 1 shot at #{input1} and hit!"
+
+			@submarine2 -= 1
+
+			if @submarine2 == 0
+				@player1.puts "Player 2's Submarine has been sunk!"
+				@player2.puts "Your Submarine has been destroyed!"
+				@numboats2 -= 1
+			end
+		elsif @board2[row][column] == 'd'
+			@player1.puts "Shot Hit!"
+			@player2.puts "Player 1 shot at #{input1} and hit!"
+
+			@destroyer2 -= 1
+
+			if @destroyer2 == 0
+				@player1.puts "Player 2's Destroyer has been sunk!"
+				@player2.puts "Your Destroyer has been destroyed!"
+				@numboats2 -= 1
+			end
+		elsif @board2[row][column] == 'p'
+			@player1.puts "Shot Hit!"
+			@player2.puts "Player 1 shot at #{input1} and hit!"
+
+			@patrolboat2 -= 1
+
+			if @patrolboat2 == 0
+				@player1.puts "Player 2's Patrol Boat has been sunk!"
+				@player2.puts "Your Patrol Boat has been destroyed!"
+				@numboats2 -= 1
+			end
 		end
 		@board2[row][column] = 'x'
+
+		if @numboats2 == 0
+			@player1.puts "You WIN!!!!!"
+			@player2.puts "You LOSE!!!!!"
+			@player1.puts "Goodbye"
+			@player2.puts "Goodbye"
+		end
 	end
 
 	def player2turn()
@@ -110,11 +179,69 @@ class Match
 		if @board1[row][column] == '-'
 			@player2.puts "Shot Missed!"
 			@player1.puts "Player 2 shot at #{input1} and missed!"
-		else
+		elsif @board1[row][column] == 'c'
 			@player2.puts "Shot Hit!"
 			@player1.puts "Player 2 shot at #{input1} and hit!"
+			@carrier1 -= 1
+
+			if @carrier1 == 0
+				@player2.puts "Player 1's Carrier has been sunk!"
+				@player1.puts "Your Carrier has been destroyed!"
+				@numboats1 -= 1
+			end
+		elsif @board1[row][column] == 'b'
+			@player2.puts "Shot Hit!"
+			@player1.puts "Player 2 shot at #{input1} and hit!"
+
+			@battleship1 -= 1
+
+			if @battleship1 == 0
+				@player2.puts "Player 1's Battleship has been sunk!"
+				@player1.puts "Your Battleship has been destroyed!"
+				@numboats1 -= 1
+			end
+		elsif @board1[row][column] == 's'
+			@player2.puts "Shot Hit!"
+			@player1.puts "Player 2 shot at #{input1} and hit!"
+
+			@submarine1 -= 1
+
+			if @submarine1 == 0
+				@player2.puts "Player 1's Submarine has been sunk!"
+				@player1.puts "Your Submarine has been destroyed!"
+				@numboats1 -= 1
+			end
+		elsif @board1[row][column] == 'd'
+			@player2.puts "Shot Hit!"
+			@player1.puts "Player 2 shot at #{input1} and hit!"
+
+			@destroyer1 -= 1
+
+			if @destroyer1 == 0
+				@player2.puts "Player 1's Destroyer has been sunk!"
+				@player1.puts "Your Destroyer has been destroyed!"
+				@numboats1 -= 1
+			end
+		elsif @board1[row][column] == 'p'
+			@player2.puts "Shot Hit!"
+			@player1.puts "Player 2 shot at #{input1} and hit!"
+
+			@patrolboat1 -= 1
+
+			if @patrolboat1 == 0
+				@player2.puts "Player 1's Patrol Boat has been sunk!"
+				@player1.puts "Your Patrol Boat has been destroyed!"
+				@numboats1 -= 1
+			end
 		end
 		@board1[row][column] = 'x'
+
+		if @numboats1 == 0
+			@player2.puts "You WIN!!!!!"
+			@player1.puts "You LOSE!!!!!"
+			@player2.puts "Goodbye"
+			@player1.puts "Goodbye"
+		end
 	end
 
 	def placeboat(location, orientation, size, board, type)
@@ -125,25 +252,25 @@ class Match
 		row = number.to_i - 1
 		column = letter.ord - 65
 
-		if orientation == "Up"
+		if orientation == "up"
 			for i in 0..lastIndex do
 				board[row - i][column] = type
 			end
 		end
 
-		if orientation == "Down"
+		if orientation == "down"
 			for i in 0..lastIndex do
 				board[row + i][column] = type
 			end
 		end
 
-		if orientation == "Left"
+		if orientation == "left"
 			for i in 0..lastIndex do
 				board[row][column - i] = type
 			end
 		end
 
-		if orientation == "Right"
+		if orientation == "right"
 			for i in 0..lastIndex do
 				board[row][column + i] = type
 			end
@@ -164,8 +291,8 @@ class Match
 		puts input1
 		puts input2
 
-		@player1.puts "Select Orientation for Carrier (Size 5) (Up, Down, Left, Right)"
-		@player2.puts "Select Orientation for Carrier (Size 5) (Up, Down, Left, Right)"
+		@player1.puts "Select Orientation for Carrier (Size 5) (up, down, left, right)"
+		@player2.puts "Select Orientation for Carrier (Size 5) (up, down, left, right)"
 
 		orientation1 = @player1.gets
 		orientation2 = @player2.gets
@@ -179,94 +306,95 @@ class Match
 		#carrier is now placed print the board to represent the new state
 		printboard
 
-		# @player1.puts "Select Start Position for Battleship (Size 4) (Ranges are A - L and 1 - 10 Example: B5)"
-		# @player2.puts "Select Start Position for Battleship (Size 4) (Ranges are A - L and 1 - 10 Example: B5)"
+		@player1.puts "Select Start Position for Battleship (Size 4) (Ranges are A - L and 1 - 10 Example: B5)"
+		@player2.puts "Select Start Position for Battleship (Size 4) (Ranges are A - L and 1 - 10 Example: B5)"
 
-		# input1 = @player1.gets
-		# input2 = @player2.gets
-		# puts input1
-		# puts input2
+		input1 = @player1.gets
+		input2 = @player2.gets
+		puts input1
+		puts input2
 
-		# @player1.puts "Select Orientation for Battleship (Size 4) (Up, Down, Left, Right)"
-		# @player2.puts "Select Orientation for Battleship (Size 4) (Up, Down, Left, Right)"
+		@player1.puts "Select Orientation for Battleship (Size 4) (up, down, left, right)"
+		@player2.puts "Select Orientation for Battleship (Size 4) (up, down, left, right)"
 
-		# orientation1 = @player1.gets
-		# orientation2 = @player2.gets
-		# puts orientation1
-		# puts orientation2
+		orientation1 = @player1.gets
+		orientation2 = @player2.gets
+		puts orientation1
+		puts orientation2
 
-		# placeboat(input1, orientation1.chomp, 4, @board1)
-  #       placeboat(input2, orientation2.chomp, 4, @board2)
-		# #carrier is now placed print the board to represent the new state
-		# printboard
+		placeboat(input1, orientation1.chomp, 4, @board1, 'b')
+        placeboat(input2, orientation2.chomp, 4, @board2, 'b')
+		#carrier is now placed print the board to represent the new state
+		printboard
 
-		# @player1.puts "Select Start Position for Submarine (Size 3) (Ranges are A - L and 1 - 10 Example: B5)"
-		# @player2.puts "Select Start Position for Submarine (Size 3) (Ranges are A - L and 1 - 10 Example: B5)"
+		@player1.puts "Select Start Position for Submarine (Size 3) (Ranges are A - L and 1 - 10 Example: B5)"
+		@player2.puts "Select Start Position for Submarine (Size 3) (Ranges are A - L and 1 - 10 Example: B5)"
 
-		# input1 = @player1.gets
-		# input2 = @player2.gets
-		# puts input1
-		# puts input2
+		input1 = @player1.gets
+		input2 = @player2.gets
+		puts input1
+		puts input2
 
-		# @player1.puts "Select Orientation for Submarine (Size 3) (Up, Down, Left, Right)"
-		# @player2.puts "Select Orientation for Submarine (Size 3) (Up, Down, Left, Right)"
+		@player1.puts "Select Orientation for Submarine (Size 3) (up, down, left, right)"
+		@player2.puts "Select Orientation for Submarine (Size 3) (up, down, left, right)"
 
-		# orientation1 = @player1.gets
-		# orientation2 = @player2.gets
-		# puts orientation1
-		# puts orientation2
+		orientation1 = @player1.gets
+		orientation2 = @player2.gets
+		puts orientation1
+		puts orientation2
 
-		# placeboat(input1, orientation1.chomp, 3, @board1)
-  #       placeboat(input2, orientation2.chomp, 3, @board2)
-		# #carrier is now placed print the board to represent the new state
-		# printboard
+		placeboat(input1, orientation1.chomp, 3, @board1, 's')
+        placeboat(input2, orientation2.chomp, 3, @board2, 's')
+		#carrier is now placed print the board to represent the new state
+		printboard
 
-		# @player1.puts "Select Start Position for Destroyer (Size 3) (Ranges are A - L and 1 - 10 Example: B5)"
-		# @player2.puts "Select Start Position for Destroyer (Size 3) (Ranges are A - L and 1 - 10 Example: B5)"
+		@player1.puts "Select Start Position for Destroyer (Size 3) (Ranges are A - L and 1 - 10 Example: B5)"
+		@player2.puts "Select Start Position for Destroyer (Size 3) (Ranges are A - L and 1 - 10 Example: B5)"
 
-		# input1 = @player1.gets
-		# input2 = @player2.gets
-		# puts input1
-		# puts input2
+		input1 = @player1.gets
+		input2 = @player2.gets
+		puts input1
+		puts input2
 
-		# @player1.puts "Select Orientation for Destroyer (Size 3) (Up, Down, Left, Right)"
-		# @player2.puts "Select Orientation for Destroyer (Size 3) (Up, Down, Left, Right)"
+		@player1.puts "Select Orientation for Destroyer (Size 3) (up, down, left, right)"
+		@player2.puts "Select Orientation for Destroyer (Size 3) (up, down, left, right)"
 
-		# orientation1 = @player1.gets
-		# orientation2 = @player2.gets
-		# puts orientation1
-		# puts orientation2
+		orientation1 = @player1.gets
+		orientation2 = @player2.gets
+		puts orientation1
+		puts orientation2
 
-		# placeboat(input1, orientation1.chomp, 3, @board1)
-  #       placeboat(input2, orientation2.chomp, 3, @board2)
-		# #carrier is now placed print the board to represent the new state
-		# printboard
+		placeboat(input1, orientation1.chomp, 3, @board1, 'd')
+        placeboat(input2, orientation2.chomp, 3, @board2, 'd')
+		#carrier is now placed print the board to represent the new state
+		printboard
 
-		# @player1.puts "Select Start Position for Patrol Boat (Size 2) (Ranges are A - L and 1 - 10 Example: B5)"
-		# @player2.puts "Select Start Position for Patrol Boat (Size 2) (Ranges are A - L and 1 - 10 Example: B5)"
+		@player1.puts "Select Start Position for Patrol Boat (Size 2) (Ranges are A - L and 1 - 10 Example: B5)"
+		@player2.puts "Select Start Position for Patrol Boat (Size 2) (Ranges are A - L and 1 - 10 Example: B5)"
 
-		# input1 = @player1.gets
-		# input2 = @player2.gets
-		# puts input1
-		# puts input2
+		input1 = @player1.gets
+		input2 = @player2.gets
+		puts input1
+		puts input2
 
-		# @player1.puts "Select Orientation for Patrol Boat (Size 2) (Up, Down, Left, Right)"
-		# @player2.puts "Select Orientation for Patrol Boat (Size 2) (Up, Down, Left, Right)"
+		@player1.puts "Select Orientation for Patrol Boat (Size 2) (up, down, left, right)"
+		@player2.puts "Select Orientation for Patrol Boat (Size 2) (up, down, left, right)"
 
-		# orientation1 = @player1.gets
-		# orientation2 = @player2.gets
-		# puts orientation1
-		# puts orientation2
+		orientation1 = @player1.gets
+		orientation2 = @player2.gets
+		puts orientation1
+		puts orientation2
 
-		# placeboat(input1, orientation1.chomp, 2, @board1)
-  #       placeboat(input2, orientation2.chomp, 2, @board2)
-		# #carrier is now placed print the board to represent the new state
-		# printboard
+		placeboat(input1, orientation1.chomp, 2, @board1, 'p')
+        placeboat(input2, orientation2.chomp, 2, @board2, 'p')
+		#carrier is now placed print the board to represent the new state
+		printboard
 
 		while @numboats1 > 0 && @numboats2 > 0 do
 			puts "next phase!!!"
 			player1turn()
 			player2turn()
+			printboard()
 		end
 	end
 
@@ -276,7 +404,17 @@ class Match
 	@board2
 	@numboats1
 	@numboats2
-	
+	@carrier1
+	@battleship1
+	@submarine1
+	@destroyer1
+	@patrolboat1
+	@carrier2
+	@battleship2
+	@submarine2
+	@destroyer2
+	@patrolboat2
+
 end
 
 puts "Starting up server..."
